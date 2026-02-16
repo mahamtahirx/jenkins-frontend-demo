acb1192ed41bc5a5f6f141220f94d516a29962e2
maham <mahamtahir664@gmail.com>
2026-02-16 15:32:09 +0500

    Update frontend to DevOps themed page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>🔥 DevOps Dashboard Demo 🔥</h1>
+        <p>CI/CD Pipeline is up and running with Jenkins!</p>
+
+        <div style={{ marginTop: '20px', textAlign: 'left' }}>
+          <h2>Key DevOps Concepts:</h2>
+          <ul style={{ textAlign: 'left' }}>
+            <li>Version Control: Git & GitHub</li>
+            <li>Continuous Integration: Jenkins</li>
+            <li>Continuous Delivery / Deployment</li>
+            <li>Containerization: Docker</li>
+            <li>Infrastructure as Code: Terraform / Ansible</li>
+            <li>Monitoring & Alerts: Prometheus / Grafana</li>
+          </ul>
+        </div>
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.jenkins.io/"
           target="_blank"
           rel="noopener noreferrer"
+          style={{ marginTop: '20px', display: 'inline-block' }}
         >
-          Learn React
+          Learn Jenkins
         </a>
       </header>
     </div>
